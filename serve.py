7510138eba389357690f96d9962a8eab0fc25985
OscarH <49325613+0sc4rher@users.noreply.github.com>
2019-10-19 00:26:41 -0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,14 +1,16 @@
-from sklearn import svm
 from sklearn import datasets
 from joblib import dump, load
 from sklearn.metrics import accuracy_score
 from flask import Flask
 from flask_restful import reqparse, Api, Resource
 import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+import pandas
+import xgboost
+
 
 def get_keys():
-        clf = svm.SVC(gamma='scale')
-        clf = load('SCVTrained.joblib')
+        clf = load('modelapi.joblib')
         def keywords_api(X):
                 y_pred = str(clf.predict([X]))
                 return y_pred
